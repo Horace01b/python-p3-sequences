ad547d96618b3b772337f03ab087c5ad024aebf8
horaceo1b <horaceken0@gmail.com>
2025-08-10 20:48:22 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+
+    # First two numbers in the Fibonacci sequence
+    sequence = [0, 1]
+
+    # If length is 1, just print [0]
+    if length == 1:
+        print([0])
+        return
+
+    # Build sequence until it reaches the required length
+    while len(sequence) < length:
+        sequence.append(sequence[-1] + sequence[-2])
+
+    print(sequence)
